f2d03d04a5e6bfe90d4de118afc94b5d2780d1ee
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-10-19 11:41:41 +0100

    Update opcodes.c

--- opcodes.c
+++ opcodes.c
@@ -7,30 +7,30 @@
  */
 void push(stack_t **stack, int value)
 {
-    stack_t *newNode;
-    stack_t **stack;
+	stack_t *newNode;
+	stack_t **stack;
 
-    newNode = (stack_t *)malloc(sizeof(stack_t));
-    if (!newNode)
-    {
-        fprintf(stderr, "Error: malloc failed\n");
-        exit(EXIT_FAILURE);
-    }
+	newNode = (stack_t *)malloc(sizeof(stack_t));
+	if (!newNode)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
 
-    newNode->n = value;
-    newNode->next = 0;
+	newNode->n = value;
+	newNode->next = 0;
 
-    if (!*stack)
-    {
-        newNode->prev = 0;
-        *stack = newNode;
-    }
-    else
-    {
-        newNode->next = *stack;
-        (*stack)->prev = newNode;
-        *stack = newNode;
-    }
+	if (!*stack)
+	{
+		newNode->prev = 0;
+		*stack = newNode;
+	}
+	else
+	{
+		newNode->next = *stack;
+		(*stack)->prev = newNode;
+		*stack = newNode;
+	}
 }
 
 /**
@@ -40,10 +40,10 @@ void push(stack_t **stack, int value)
  */
 void pall(stack_t **stack, unsigned int line_number)
 {
-    stack_t *top = *stack;
-    while (top != 0)
-    {
-        printf("%d", top->n);
-        top = top->prev;
-    }
+	stack_t *top = *stack;
+	while (top != 0)
+	{
+		printf("%d", top->n);
+		top = top->prev;
+	}
 }
